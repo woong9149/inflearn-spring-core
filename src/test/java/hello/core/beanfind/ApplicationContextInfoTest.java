f05b2977fdca4e0af87f3a3c1ea8eda8a6fe4c1e
woong9149 <sewoong0409@naver.com>
2022-03-09 21:00:21 +0900

    20220309

---
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,47 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + " object = " + bean);
+
+            /**
+             * ac.getBeanDefinitionNames() : 스프링에 등록된 모든 빈 이름을 조회한다.
+             * ac.getBean(): 빈 이름으로 빈 객체(인스턴스)를 조회한다.
+             */
+        }
+    }
+
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            /**
+             * - Role ROLE_APPLICATION: 직접 등록한 애플리케이션 빈
+             * - Role ROLE_INFRASTRUCTURE: 스프링이 내부에서 사용하는 빈
+             * - 스프링 내부에서 사용하는 빈은 getRole()로 구분할 수 있다.
+             */
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + " object = " + bean);
+            }
+
+        }
+    }
+}
