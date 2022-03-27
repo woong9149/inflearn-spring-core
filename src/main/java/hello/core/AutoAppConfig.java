83a0ea2be639cfb8656210956591ee0401843fa7
woong9149 <sewoong0409@naver.com>
2022-03-27 21:08:15 +0900

    20220327

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        basePackages = "hello.core.member",
+//        basePackages = "hello.core.member",
 //        basePackages = {"hello.core.member", "hello.core.service"}, 여러 시작 위치 지정할 때
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
